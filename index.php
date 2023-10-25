bb0cbabdd41de9c3b7c7ea31d0943135c8a0a2c0
basiaszafraniec <basia.szafraniec@gmail.com>
2023-10-25 12:28:33 +0200

    basic layout

---
+++ index.php
@@ -0,0 +1,37 @@
+<!-- main page -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ravnsbjerg Info Screen</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <?php include("header.php"); ?>
+    <div id="main_links">
+        <div class="row">
+            <a href="map.php">
+                <div id="map" class="main_link">map</div>
+            </a>
+            <a href="contacts.php">
+                <div id="contacts" class="main_link">contacts</div>
+            </a>
+        </div>
+        <div class="row">
+            <a href="facilities.php">
+                <div id="facilities" class="main_link">facilities</div>
+            </a>
+            <a href="events.php">
+                <div id="events" class="main_link">events</div>
+            </a>
+        </div>
+
+
+    </div>
+    <script src="script.js"></script>
+</body>
+
+</html>
