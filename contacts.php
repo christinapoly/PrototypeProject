1b8714326748e298cf4c59c2a76ca3cd033632e3
basiaszafraniec <basia.szafraniec@gmail.com>
2023-10-26 11:00:45 +0200

    idk

--- contacts.php
+++ contacts.php
@@ -1,3 +1,4 @@
+<!-- mihaela -->
 <!DOCTYPE html>
 <html lang="en">
 
--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hi");
+const scrollContainer = document.querySelector('.scroll-container');
+const scrollContent = document.querySelector('.scroll-content');
+const scrollLeft = document.querySelector('.scroll-left');
+const scrollRight = document.querySelector('.scroll-right');
+
+scrollLeft.addEventListener('click', () => {
+    scrollContainer.scrollLeft -= 100; // Adjust the scroll amount as needed
+});
+
+scrollRight.addEventListener('click', () => {
+    scrollContainer.scrollLeft += 100; // Adjust the scroll amount as needed
+});
