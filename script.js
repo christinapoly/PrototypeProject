71779ea04a92876342a2b62024014fe980c50f19
basiaszafraniec <basia.szafraniec@gmail.com>
2023-11-03 14:58:47 +0100

    test also

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-const scrollContainer = document.querySelector('.scroll-container');
-const scrollContent = document.querySelector('.scroll-content');
-const scrollLeft = document.querySelector('.scroll-left');
-const scrollRight = document.querySelector('.scroll-right');
-
-scrollLeft.addEventListener('click', () => {
-    scrollContainer.scrollLeft -= 100; // Adjust the scroll amount as needed
-});
-
-scrollRight.addEventListener('click', () => {
-    scrollContainer.scrollLeft += 100; // Adjust the scroll amount as needed
-});
+function createHeader(){
+    const header = document.getElementById("header1");
+    
+    
+}
