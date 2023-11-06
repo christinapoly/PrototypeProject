c0960109d7785911ee6476bf8511f0bb75cb6d21
basiaszafraniec <basia.szafraniec@gmail.com>
2023-11-06 12:39:02 +0100

    YES

--- script.js
+++
@@ -1,8 +0,0 @@
-function createHeader(){
-    const header = document.getElementById("header1");
-    
-    const arrow_back = new Image();
-    arrow_back.src = "images/arrow_back.png";
-
-    header.appendChild(arrow_back);
-}
