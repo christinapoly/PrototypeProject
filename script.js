b55775dbb56d61c88484a90c9d882f28a21cc8e1
basiaszafraniec <basia.szafraniec@gmail.com>
2023-11-06 11:02:05 +0100

    main, events and some other stuff sprry i forgot to commit in a while lol

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function createHeader(){
     const header = document.getElementById("header1");
     
-    
+    const arrow_back = new Image();
+    arrow_back.src = "images/arrow_back.png";
+
+    header.appendChild(arrow_back);
 }
